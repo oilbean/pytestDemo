c8090663bb14b1c84268eeaad8172d837cfe77b4
oilbean <1057260346@qq.com>
2023-03-15 23:15:03 +0800

    commit

--- testCase/demoCase1.py
+++ testCase/demoCase1.py
@@ -1,10 +1,30 @@
 
 import pytest
 
+from common import getData,getResultText
+from common.re import request
+
+
 class TestCase():
 
-    def setup_class(cls):
+    @classmethod
+    def setup(cls):
         pass
 
-    def teardown_class(cls):
-        pass
+    data = getData.getYamlData("../caseData/pop/popup.yaml")
+
+    @pytest.mark.parametrize('case',data)
+    def test_case01(self,case):
+
+        method = case['http']['method']
+        url = case['http']['path']
+        data = case['data']
+
+        result = request().request(url=url,method=method,data=data)
+
+        popupType = getResultText(result,"popupType")
+
+        expected = case['expected']['popupType']
+
+        assert popupType == expected
+
